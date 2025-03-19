8311b8267dd66cd29e4772053b971d48a779ad8e
Rodion <rodionchik17@gmail.com>
2025-03-20 01:54:39 +0200

    filling class

--- microservice-order/src/main/java/microservice/order/microserviceorder/service/OrderService.java
+++ microservice-order/src/main/java/microservice/order/microserviceorder/service/OrderService.java
@@ -1,7 +1,20 @@
 package microservice.order.microserviceorder.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public class OrderService {
+@Slf4j
+public class OrderService
+{
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
 }
