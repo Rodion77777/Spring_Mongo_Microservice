8d044a016a8970d4a04bab928aa2fe8f7c63f37d
Rodion <rodionchik17@gmail.com>
2025-03-21 19:11:45 +0200

    copied config classes

---
+++ microservice-order/src/main/java/microservice/order/microserviceorder/config/SpringFoxConfig.java
@@ -0,0 +1,25 @@
+package microservice.order.microserviceorder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    // ➜ Создаёт и настраивает Docket — основной объект SpringFox для генерации Swagger-документации.
+    @Bean
+    public Docket api() {
+        // ➜ Указывает, что используется Swagger 2.
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                // ➜ Включает в документацию только классы с аннотацией @RestController.
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                // ➜ Включает все эндпоинты.
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
