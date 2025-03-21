2bbc6ba72710315494b4d6c206e86c48c3dfe451
Rodion <rodionchik17@gmail.com>
2025-03-21 19:56:26 +0200

    created classes exceptions and configs, utils

---
+++ microservice-order/src/main/java/microservice/order/microserviceorder/exceptions/FieldErrorVM.java
@@ -0,0 +1,18 @@
+package microservice.order.microserviceorder.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Getter
+public class FieldErrorVM implements Serializable
+{
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private final String objectName;
+    private final String field;
+    private final String message;
+}
