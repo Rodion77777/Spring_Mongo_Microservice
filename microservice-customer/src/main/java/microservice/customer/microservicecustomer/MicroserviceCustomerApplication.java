9d357aa97a54cbc28277f1bd7ba1cc41ae7e56d4
Rodion <rodionchik17@gmail.com>
2025-03-26 03:31:35 +0200

    added annotation @EnableMongoAuditing

--- microservice-customer/src/main/java/microservice/customer/microservicecustomer/MicroserviceCustomerApplication.java
+++ microservice-customer/src/main/java/microservice/customer/microservicecustomer/MicroserviceCustomerApplication.java
@@ -2,8 +2,10 @@ package microservice.customer.microservicecustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class MicroserviceCustomerApplication {
 
     public static void main(String[] args) {
